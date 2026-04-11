f83bae75f0309c44a7c4a27d46a549b95e897a8d
not-1not <adhyap11@gmail.com>
2026-04-11 11:37:52 +0700

    fix: set base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: process.env.VERCEL ? '/' : (mode === 'production' ? '/TKA_Toni/' : '/'),
+  base: process.env.VERCEL ? '/' : (mode === 'production' ? '/TKA_pewe3/' : '/'),
 }))
